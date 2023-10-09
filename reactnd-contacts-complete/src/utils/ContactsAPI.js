a70d427f2a8c5f30dc7583ea7f88ee36ee385799
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-09 23:02:59 +0700

    add env for frontend

--- reactnd-contacts-complete/src/utils/ContactsAPI.js
+++ reactnd-contacts-complete/src/utils/ContactsAPI.js
@@ -1,4 +1,4 @@
-const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://demo-env.eba-9werppqp.us-east-1.elasticbeanstalk.com'
+const api = process.env.REACT_APP_CONTACTS_API_URL || 'http://demo-env.eba-cqryxvfx.us-east-1.elasticbeanstalk.com'
 
 let token = localStorage.token
 
