d511b55cadc81ec2ec9f19972e8c5dbdeb00b9e8
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-09 20:14:09 +0700

    Fix error eb init

--- reactnd-contacts-server/bin/deploy.sh
+++ reactnd-contacts-server/bin/deploy.sh
@@ -1,5 +1,3 @@
-
-eb init
+eb create --cfg demo-env
 # Verify the Node.js version and region in .elasticbeanstalk/config.yml file
-eb setenv AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID  AWS_BUCKET=$AWS_BUCKET  AWS_DEFAULT_REGION=$AWS_DEFAULT_REGION  AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
 eb create --sample --single --instance-types t2.small
