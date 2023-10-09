ff7ea1772a4e09a180556d572de40dd7716bca08
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-09 13:46:01 +0700

    Fix source Permission denied

--- reactnd-contacts-server/bin/installEB.sh
+++ reactnd-contacts-server/bin/installEB.sh
@@ -6,4 +6,5 @@ sudo apt-get install python3 python3-pip
 python3 -m pip install virtualenv
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 python3 ./aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
-export PATH="/home/circleci/.ebcli-virtual-env/executables:$PATH"'
+echo 'export PATH="/home/circleci/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile
+. ~/.bash_profile
