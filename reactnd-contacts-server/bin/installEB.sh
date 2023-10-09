f6f263a91e5824eb8a44f94eb1d7b2d9e258630a
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-09 10:09:53 +0700

    Fix python not found

--- reactnd-contacts-server/bin/installEB.sh
+++ reactnd-contacts-server/bin/installEB.sh
@@ -7,4 +7,3 @@ python3 -m pip install virtualenv
 git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
 python3 ./aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
 echo 'export PATH="/root/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && source ~/.bash_profile
-eb --version
