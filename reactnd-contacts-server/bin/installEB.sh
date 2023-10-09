f3c6c1de4b093d7ef9c0dd121a47c16445067800
NguyenCongAnh1 <nguyenanh342000@gmail.com>
2023-10-09 16:11:53 +0700

    Fix source Permission denied

--- reactnd-contacts-server/bin/installEB.sh
+++ reactnd-contacts-server/bin/installEB.sh
@@ -1,10 +1,10 @@
-cd
-sudo apt-get update
-sudo apt-get install python3-pip
-sudo apt-get install virtualenv
-sudo apt-get install python3 python3-pip
-python3 -m pip install virtualenv
-git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
-python3 ./aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
-. ./setup.bash
-echo 'export PATH="/root/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && . ~/.bash_profile
+# cd
+# sudo apt-get update
+# sudo apt-get install python3-pip
+# sudo apt-get install virtualenv
+# sudo apt-get install python3 python3-pip
+# python3 -m pip install virtualenv
+# git clone https://github.com/aws/aws-elastic-beanstalk-cli-setup.git
+# python3 ./aws-elastic-beanstalk-cli-setup/scripts/ebcli_installer.py
+# . ./setup.bash
+# echo 'export PATH="/root/.ebcli-virtual-env/executables:$PATH"' >> ~/.bash_profile && . ~/.bash_profile
